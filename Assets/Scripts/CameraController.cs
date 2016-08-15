972bec583da37c7a1b49b32e822a1834fff7762f
Jason Malia <jmalia3@gatech.edu>
2016-08-15 14:36:22 -0400

    Add camera movement (locked in 3rd person view to Player via script)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate() is best for follow cameras, procedural animation, and gathering last known info
+	// guaranteed to run after Update() is complete
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
